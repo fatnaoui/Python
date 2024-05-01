69a5d54a73ec6d45c07343bd6c6234651d1f12ce
Hamza Fatnaoui <hamzafatnaoui.ai@gmail.com>
2024-05-01 11:10:18 +0100

    Creating 2 models "Article" and "Tag". After that migrating the changes. then creating a serializer for our Article class with Meta class and Modelserializer. Finally creating a super user to add an article "Python Basics" to the database.

--- 49-RestAPI/api/models.py
+++ 49-RestAPI/api/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+class Tag(models.Model):
+    name=models.CharField(max_length=120)
+
+class Article(models.Model):
+    slug=models.SlugField(null=False,blank=False)  # not null in the database and not empty in frontend
+    title=models.CharField(max_length=256)
+    description=models.TextField()
+    body=models.TextField()
+    tags=models.ManyToManyField('Tag',blank=True)
+    createdAt=models.DateTimeField(auto_now_add=True)
+    updatedAt=models.DateTimeField(auto_now_add=True)
+    favorited=models.BooleanField()
+    favoritesCount=models.IntegerField(default=0)
+    def __str__(self):
+        return self.title
--- 49-RestAPI/api/serializers.py
+++ 49-RestAPI/api/serializers.py
@@ -1,6 +1,12 @@
 from rest_framework import serializers
+from api import models
 
 class StudentSerializer(serializers.Serializer):
     name=serializers.CharField(max_length=100)
     id=serializers.IntegerField()
     mark=serializers.IntegerField()
+
+class ArticleSerializer(serializers.ModelSerializer):
+    class Meta:
+        model=models.Article
+        field='__all__'
