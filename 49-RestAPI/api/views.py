2d9a2a2762b76247a8ba4341492ff7603e895d80
Hamza Fatnaoui <hamzafatnaoui.ai@gmail.com>
2024-05-01 15:47:30 +0100

    You can with these generic views ["ListCreateAPIView","RetrieveUpdateAPIView"] to do multiple functionalities with get (list one or all the instances), post ( with create another instance) and patch (update the instance).

--- 49-RestAPI/api/views.py
+++ 49-RestAPI/api/views.py
@@ -4,13 +4,18 @@ from rest_framework.decorators import api_view
 from rest_framework.request import Request
 from rest_framework.response import Response
 from api import serializers,models
-from rest_framework.generics import RetrieveAPIView,ListAPIView
+from rest_framework.generics import (
+    RetrieveAPIView,
+    ListAPIView,
+    ListCreateAPIView,   # you can with it list and also create a view based on inputs
+    RetrieveUpdateAPIView  # it can show a view and also update it with patch method
+)
 
-class ArticleListView(ListAPIView):
+class ArticleListView(ListCreateAPIView):
     queryset=models.Article.objects.all()
     serializer_class=serializers.ArticleSerializer
 
-class ArticleDetailtView(RetrieveAPIView):
+class ArticleDetailtView(RetrieveUpdateAPIView):
     queryset=models.Article.objects.all()
     serializer_class=serializers.ArticleSerializer
 
